707ba5e1f6e65a2d27dc77ed5a6522db4c3039ab
SUDIP KUNDU <sudip2003kundu@gmail.com>
2024-09-26 22:03:25 +0530

    removed

--- server/build/graphql/query/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Query = void 0;
-const typedefs_1 = require("./typedefs");
-const resolvers_1 = require("./resolvers");
-const queries_1 = require("./queries");
-exports.Query = { queries: queries_1.queries, typeDefs: typedefs_1.typeDefs, resolvers: resolvers_1.resolvers };
